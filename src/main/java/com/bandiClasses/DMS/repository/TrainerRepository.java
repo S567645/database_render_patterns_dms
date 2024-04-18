4cd0bef4b063112b596852c5ae50a6263a1bbe7e
sumedh <143051578+S567645@users.noreply.github.com>
2024-04-18 16:20:35 -0500

    done

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author S567645
+ */
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
